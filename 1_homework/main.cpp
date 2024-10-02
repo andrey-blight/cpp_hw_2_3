b208e1220ffe019df444bd11f9869a42fa346274
andrey-blight <kizhinov05@gmail.com>
2024-10-02 10:44:59 +0300

    First version

---
+++ 1_homework/main.cpp
@@ -0,0 +1,46 @@
+#include "statement.h"
+#include <iostream>
+
+template<auto func>
+class BinaryOp : public Statement {
+public:
+    BinaryOp() : Statement(2, 1, true) {};
+
+    [[nodiscard]] std::vector<int> apply(std::vector<int> in) const override {
+        if (in.size() < 2) {
+            std::cout << "BinaryOp Error: not enough arguments" << std::endl;
+            exit(1);
+        }
+        int result = func(in[0], in[1]);
+        in.pop_back();
+        in.pop_back();
+        in.push_back(result);
+        return in;
+    }
+};
+
+template<int number>
+class NumberOp : public Statement {
+    NumberOp() : Statement(0, 1, true) {};
+
+    [[nodiscard]] std::vector<int> apply(std::vector<int> in) const override {
+        in.push_back(number);
+        return in;
+    }
+};
+
+std::vector<std::shared_ptr<Statement>> compile(std::string_view str) {
+    std::vector<std::shared_ptr<Statement>> statements{};
+    if (str == "+") {
+        BinaryOp<std::plus<int>{}> op{};
+        statements.push_back(std::shared_ptr<Statement>{&op});
+    }
+
+    return statements;
+}
+
+int main() {
+    for (auto &el: compile("+")) {
+        std::cout << el->is_pure() << " " << el->get_arguments_count() << " " << el->get_results_count() << " ";
+    }
+}
